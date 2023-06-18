2430d0708f345deb5d81d535d64378873ae50ad7
snsakib <hello@snsakib.com>
2023-06-18 14:18:43 +0600

    initial project setup

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    testnet: {
+      url: `${process.env.POLYGON_MUMBAI_TESTNET_URL}`,
+      accounts: [`${process.env.ACCOUNTS_PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: `${process.env.POLYGON_MAINNET_URL}`,
+      accounts: [`${process.env.ACCOUNTS_PRIVATE_KEY}`],
+    },
+  },
+};
