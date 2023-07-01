bf97f4a79c0c2d14bf141ddb46e77970fdec28b3
snsakib <hello@snsakib.com>
2023-07-01 06:48:42 +0600

    remove unused imports

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,10 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFTMarketplace is ERC721URIStorage {
     using Counters for Counters.Counter;
